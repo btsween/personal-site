4e69e181d5c227ae46ddfd6071ea7ae867de5ab0
btsween <47014701+btsween@users.noreply.github.com>
2021-02-25 16:00:41 -0500

    REFACTOR: remove all traces of CIC (#3)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
-import Footer from "./components/navigation/Footer";
 import Home from "./components/Home";
-import Calculator from "./components/Calculator";
-import TopNavigation from "./components/navigation/TopNavigation";
 
 class App extends Component {
   render() {
@@ -12,12 +9,7 @@ class App extends Component {
       <BrowserRouter>
         <div className="App">
           <Switch>
-            <Route path="/" component={Home} exact />
-            <Route
-              path="/compound-interest-calculator"
-              component={Calculator}
-              exact
-            />
+            <Route path="/" component={Home} />
           </Switch>
         </div>
       </BrowserRouter>
